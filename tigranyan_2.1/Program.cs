70559cfb361a7e2363ce8646ac5e9e522bbb2d0b
Serj Tigranyan <serj-tigranyan@mail.ru>
2021-10-25 13:15:28 +0200

    Добавьте файлы проекта.

---
+++ tigranyan_2.1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace tigranyan_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите координату x:");
+            double x = double.Parse(Console.ReadLine());
+            Console.WriteLine("введите координату y:");
+            double y = double.Parse(Console.ReadLine());
+            if (y >= -x + 1 && y <= -x + 2 && y <= x - 1 && y >= x - 2) Console.WriteLine("Точка попадает в область.");
+            else Console.WriteLine("Точка не попадает в область.");
+        }
+    }
+}
